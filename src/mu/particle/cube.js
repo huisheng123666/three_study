5e9a7709001afb56e54975e14980f23000c466b8
xmw <marvin@hbtgft.com>
2023-02-23 16:28:02 +0800

    feat: sprite toggle

--- src/mu/particle/cube.js
+++ src/mu/particle/cube.js
@@ -1,13 +1,14 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as BufferGeometryUtils from "three/examples/jsm/utils/BufferGeometryUtils.js";
+import gsap from 'gsap'
+import * as dat from 'dat.gui'
 // 创建一个场景
 const scene = new THREE.Scene();
 
 // 创建一个相机 视点
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 // 设置相机的位置
-camera.position.set(0,30,100);
+camera.position.set(200,300,300);
 camera.lookAt(new THREE.Vector3(0, 0, 0))
 
 // 创建一个渲染器
@@ -49,32 +50,90 @@ function getSprite() {
   return texture
 }
 
-let geometry = new THREE.BoxGeometry(10 , 10, 10, 10, 10, 10)
+const points = []
+const oldPos = []
+const newPos = []
 
-function createPointCloud() {
 
-  geometry.deleteAttribute( 'normal' );
-  geometry.deleteAttribute( 'uv' );
-
-  geometry = BufferGeometryUtils.mergeVertices(geometry);
-
-  const material = new THREE.PointsMaterial({
-    size: 2,
-    transparent: true,
-    map: getSprite()
-  })
-
-  const cloud = new THREE.Points(geometry, material)
-
-  scene.add(cloud)
-
-  console.log(geometry)
+function createNormalSprite(type) {
+  for (let i = -5; i < 5; i++) {
+    for (let j = -5; j < 5; j++) {
+      const material = new THREE.SpriteMaterial({
+        map: getSprite()
+      })
+      const sprite = new THREE.Sprite(material)
+      switch(type) {
+        case 1:
+          sprite.position.set(i * 10, j * 10, -50)
+          break
+        case 2:
+          sprite.position.set(i * 10, j * 10, 50)
+          break
+        case 3:
+          sprite.position.set(i * 10, 40, j * 10)
+          break
+        case 4:
+          sprite.position.set(i * 10, -50, j * 10)
+          break
+        case 5:
+          sprite.position.set(50, i * 10, j * 10)
+          break
+        case 6:
+          sprite.position.set(-50, i * 10, j * 10)
+          break
+      }
+      sprite.scale.set(2, 2, 2)
+      points.push(sprite)
+      oldPos.push([sprite.position.x, sprite.position.y, sprite.position.z])
+      scene.add(sprite)
+    }
+  }
 }
 
-createPointCloud()
+createNormalSprite(1)
+createNormalSprite(2)
+createNormalSprite(3)
+createNormalSprite(4)
+createNormalSprite(5)
+createNormalSprite(6)
+
+const range = 800
+
+function randomPosition() {
+  points.forEach(item => {
+    const pos = [Math.random() * range - range / 2, Math.random() * range - range / 2, Math.random() * range - range / 2]
+    item.position.set(...pos)
+    newPos.push(pos)
+  })
+}
+
+randomPosition()
+
+function open(open) {
+  points.forEach((item, index) => {
+    const pos = open ? newPos : oldPos
+    gsap.to(item.position, {
+      x: pos[index][0],
+      y: pos[index][1],
+      z: pos[index][2],
+      duration: 2,
+      repeat: 0,
+      // yoyo: true,
+      ease: 'power2'
+    })
+  })
+}
+
+const gui = new dat.GUI()
+
+gui.add({
+  san: true
+}, 'san').onChange((value) => {
+  open(value)
+})
 
 const animation = () => {
-  scene.rotation.y += 0.01
+  // scene.rotation.y += 0.01
   orbitControls.update()
 
   camera.updateProjectionMatrix()
