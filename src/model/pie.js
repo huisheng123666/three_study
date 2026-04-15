c0ad5519bffcca8e592ae4e1ab0fe875dc12f70c
xmw <marvin@hbtgft.com>
2026-04-15 11:26:41 +0800

    feat: pie

--- src/model/pie.js
+++ src/model/pie.js
@@ -9,10 +9,10 @@ var scene = new THREE.Scene();
 
 // 创建一个摄像机
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 10;
+camera.position.set(0, 10, 10);
 
 // 创建一个渲染器
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
@@ -36,7 +36,10 @@ document.body.appendChild( renderer.domElement );
 
 const geometry2 = new THREE.CylinderGeometry( 1.3, 1.3, 2, 32, 10);
 
-const material = new THREE.MeshNormalMaterial(  );
+const material = new THREE.MeshMatcapMaterial({
+  color: 0xff0000,
+})
+
 
 const brush2 = new Brush(geometry2, material);
 brush2.updateMatrixWorld();
@@ -61,21 +64,22 @@ const extrudeSettings = {
 
 const geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
 geometry.rotateX(Math.PI/2)
-const brush3 = new Brush( geometry, material ) ;
+const brush3 = new Brush(geometry, material) ;
 brush3.position.y = 1
 brush3.updateMatrixWorld();
 const result2 = evaluator.evaluate( brush3, brush2, SUBTRACTION );
+console.log(result2)
 scene.add(result2);
+
+const ambientLight = new THREE.AmbientLight(0x404040, 100);
+scene.add(ambientLight);
 
 const orbitControls = new OrbitControls(camera, renderer.domElement)
 
 // 渲染场景
 function animate() {
   requestAnimationFrame( animate );
-  // outerCylinder.rotation.x += 0.01;
-  // outerCylinder.rotation.y += 0.01;
-  // innerCylinder.rotation.x += 0.01;
-  // innerCylinder.rotation.y += 0.01;
   renderer.render( scene, camera );
+  orbitControls.update()
 }
 animate();
